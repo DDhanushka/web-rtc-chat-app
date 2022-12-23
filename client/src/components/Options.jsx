370816f9e35336e039e6c414db71597980224049
DDhanushka <dimuthudhanushka8@gmail.com>
2022-12-23 17:35:06 +0530

    basic video player

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { SocketContext } from "../SocketContext";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Options = ({ children }) => {
-  return <div>Options {children}</div>;
+	const { me, callAccepted, name, setName, leaveCall, callUser, callEnded } =
+		useContext(SocketContext);
+	const [idToCall, setIdToCall] = useState("");
+
+	return (
+		<div className="flex flex-row">
+			<div>
+				<input
+					className=" border-solid border-2 border-blue-500"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<p>Account info</p>
+				<CopyToClipboard text={me} onCopy={() => null}>
+					<button className="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded">
+						Copy ID
+					</button>
+				</CopyToClipboard>
+			</div>
+			<div>
+				<p>Make a call</p>
+				<input
+					className=" border-solid border-2 border-blue-500"
+					value={idToCall}
+					onChange={(e) => setIdToCall(e.target.value)}
+				/>
+				{callAccepted && !callEnded ? (
+					<button
+						onClick={leaveCall}
+						className="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded"
+					>
+						hang up
+					</button>
+				) : (
+					<button
+						onClick={() => callUser(idToCall)}
+						className="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded"
+					>
+						call
+					</button>
+				)}
+				{children}
+			</div>
+		</div>
+	);
 };
 
 export default Options;
